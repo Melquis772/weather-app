c8c87288ce963a0ab37e7134901cdeb382cfa70f
Melquisedec <melquis772@gmail.com>
2022-09-21 17:45:38 -0400

    updated api

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -4,7 +4,6 @@ const fetchApi = async (cityName, units) => {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=${units}`)
         const data = await response.json()
 
-        console.log(data)
         return data
 
     } catch (error) {
